88a2c628525375cfe01acc725c69c879cbc0865c
padda76 <odd.hartvigsen@gmail.com>
2020-04-05 13:55:05 +0200

    added createComment

---
+++ graphql/resolvers/comments.js
@@ -0,0 +1,32 @@
+const { UserInputError } = require('apollo-server');
+
+const Post = require('../../models/Post');
+const checkAuth = require('../../util/check-auth');
+
+module.exports = {
+    Mutation: {
+        createComment: async (_, {postId, body}, context) => {
+            const { username } = checkAuth(context);
+
+            if(body.trim() === ''){
+                throw new UserInputError('Empty comment',{
+                    errors: {
+                        body: 'Comments can not be empty'
+                    }
+                })
+            }
+
+            const post = await Post.findById(postId);
+            if(post){
+                post.comments.unshift({
+                    body,
+                    username,
+                    createdAt : new Date().toISOString()
+                })
+
+                post.save();
+                return post;
+            } else throw new UserInputError('Post not found');
+        }
+    }
+}
